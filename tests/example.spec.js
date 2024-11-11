dc2a69cc8506e0230818c8baa7f3e6cb6a98717b
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-11 14:25:27 +0100

    Test radiobuttons, assert

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,7 +8,13 @@ test('UI controls', async ({ page }) => {
   const password = page.locator("#password");
   const signInButton = page.locator("#signInBtn");
   const roleDropdown = page.locator("select.form-control");
+  const isAdmin = page.locator("span.checkmark").nth(1);
 
   await roleDropdown.selectOption("consult");
+
+  await isAdmin.click();
+  await page.locator("#okayBtn").click();
+  await expect(isAdmin).toBeChecked();
+  console.log(await isAdmin.isChecked());
   await page.pause();
 });
